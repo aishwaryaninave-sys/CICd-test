a6126ee3281158abc6302f0816d25f9ac463b938
aishwaryaninave-sys <aishwarya.ninave@gmail.com>
2026-02-01 10:30:06 +0530

    Fix import statement and formatting in test_Calculator

--- test/test_Calculator.py
+++ test/test_Calculator.py
@@ -1,7 +1,13 @@
-from app.calculator import add,substractor
+from app.calculator import add,substract
 
+'''
+  from app -> folder
+  calculator -> fileName
+  import add,substract (functions)
+'''
 def test_add():
   assert add(2,3) == 5
 
+
 def test_substract():
-  assert substract (5,3) == 2
+  assert substract(5,3) == 2
